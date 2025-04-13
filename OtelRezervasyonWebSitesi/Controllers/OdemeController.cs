3a255062347cffcc26c2ec8edb82579374bdd6a9
Emir Görkem Özdemir <Emir Grkem zdemir@DESKTOP-5TA2SUU>
2025-04-13 15:56:12 +0300

    Proje dosyası ekle.

---
+++ OtelRezervasyonWebSitesi/Controllers/OdemeController.cs
@@ -0,0 +1,22 @@
+﻿using OtelRezervasyonWebSitesi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OtelRezervasyonWebSitesi.Controllers
+{
+    public class OdemeController : Controller
+    {
+        // GET: Odeme
+        OtelRezervasyonDBEntities db = new OtelRezervasyonDBEntities();
+
+        [HttpGet]
+        public ActionResult ListPays()
+        {
+            List<Odemeler> pays = db.Odemelers.ToList();
+            return View(pays);
+        }
+    }
+}
---
+++ OtelRezervasyonWebSitesi/Controllers/PersonController.cs
@@ -0,0 +1,21 @@
+﻿using OtelRezervasyonWebSitesi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OtelRezervasyonWebSitesi.Controllers
+{
+    public class PersonController : Controller
+    {
+        OtelRezervasyonDBEntities db = new OtelRezervasyonDBEntities();
+
+        [HttpGet]
+        public ActionResult ListPerson()
+        {
+            List<Person> persons = db.People.ToList();
+            return View(persons);
+        }
+    }
+}
